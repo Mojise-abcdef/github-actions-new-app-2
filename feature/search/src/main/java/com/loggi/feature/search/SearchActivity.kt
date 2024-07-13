686824061ecebee4abe83db3ef0dc3f8b017b28e
dev-loggi <jsl663663@gmail.com>
2024-07-14 01:27:39 +0900

    feat(search): 기능 추가 (Glide 4.14.2)

--- feature/search/src/main/java/com/loggi/feature/search/SearchActivity.kt
+++ feature/search/src/main/java/com/loggi/feature/search/SearchActivity.kt
@@ -18,10 +18,12 @@ class SearchActivity : AppCompatActivity() {
 
         function1()
         function2()
+        function3WithGlide()
     }
 
     private fun function1() = Log.d(TAG, "function1()")
     private fun function2() = Log.d(TAG, "function2()")
+    private fun function3WithGlide() = Log.d(TAG, "function3ForGlide()")
 
     companion object {
         private const val TAG = "SearchActivity"
