1803ae961a976572a2248b5fca9f0ab07d36a1b5
dev-loggi <jsl663663@gmail.com>
2024-07-14 00:56:29 +0900

    feat(search): ":feature:search" 모듈 추가

--- feature/search/src/main/java/com/loggi/feature/search/SearchActivity.kt
+++ feature/search/src/main/java/com/loggi/feature/search/SearchActivity.kt
@@ -1,6 +1,7 @@
 package com.loggi.feature.search
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.loggi.feature.search.databinding.ActivitySearchBinding
@@ -14,5 +15,15 @@ class SearchActivity : AppCompatActivity() {
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_search)
         binding.lifecycleOwner = this
+
+        function1()
+    }
+
+    private fun function1() {
+        Log.d(TAG, "function1()")
+    }
+
+    companion object {
+        private const val TAG = "SearchActivity"
     }
 }
